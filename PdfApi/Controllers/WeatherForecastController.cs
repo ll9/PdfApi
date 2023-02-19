7131acb701e6484481801f03b39c4aa68c45fd9f
ll9 <lwierer@web.de>
2023-02-19 20:19:22 +0100

    wip

--- PdfApi/Controllers/WeatherForecastController.cs
+++ PdfApi/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using PuppeteerSharp;
 
 namespace PdfApi.Controllers
 {
@@ -18,16 +19,61 @@ namespace PdfApi.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet("1")]
+        public async Task<ActionResult> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var html = "<h1>bla</h1>";
+            var stream = await GetHtmlStream(html);
+            return File(stream, "application/pdf");
+        }
+
+        [HttpGet("3")]
+        public async Task<ActionResult> Get3()
+        {
+            var html = """
+            <table style="border-collapse: collapse; width: 100%; border-width: 1px; background-color: #ECF0F1;" border="1">
+                <colgroup>
+                    <col style="width: 50%;">
+                    <col style="width: 50%;">
+                </colgroup>
+                <tbody>
+                    <tr>
+                        <td style="border-width: 1px;">Nummer</td>
+                        <td style="border-width: 1px;">02</td>
+                    </tr>
+                    <tr>
+                        <td style="border-width: 1px;">Bezeichnung</td>
+                        <td style="border-width: 1px;">Wohngeb&auml;ude</td>
+                    </tr>
+                </tbody>
+            </table>
+            """;
+            var stream = await GetHtmlStream(html);
+            return File(stream, "application/pdf");
+        }
+
+        [HttpGet("2")]
+        public async Task<ActionResult> Get2()
+        {
+            var html = """
+               <h1 style="color: red; margin-left: 10px; background: blue">bla</h1>
+               <p>This is <strong>important</strong></p>
+            """;
+            var stream = await GetHtmlStream(html);
+            return File(stream, "application/pdf");
+        }
+
+        private async Task<Stream> GetHtmlStream(string html)
+        {
+            var browserFetcher = new BrowserFetcher();
+            await browserFetcher.DownloadAsync();
+
+            await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true });
+
+            await using var page = await browser.NewPageAsync();
+            await page.SetContentAsync(html);
+
+            return await page.PdfStreamAsync();
         }
     }
 }
